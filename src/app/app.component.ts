a61ec040d177677831afd3d97cff5e3914addd3b
Matheus Ribeiro <github.matheusribeiro@gmail.com>
2025-01-12 17:48:51 -0300

    feat: criar header e footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent, HeaderComponent } from './shared/components';
 
 @Component({
   selector: 'dbd-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
