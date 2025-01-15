d7e60284c01c6b39aecedad2d52260248e5c4a1b
Matheus Ribeiro <github.matheusribeiro@gmail.com>
2025-01-14 22:51:17 -0300

    feat: rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'game/characters',
+    loadComponent: () => import('./features/characters/characters.component').then(c => c.CharactersComponent)
+  }
+];
