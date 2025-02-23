e394e80fca0f8240d08a2d436aaae2e100f0251b
Matheus Silva <github.matheusribeiro@gmail.com>
2025-02-22 22:39:02 -0300

    fix: correcao url producao

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 import { IEnvironment } from "./interfaces/environment.interface";
 
 export const environment: IEnvironment = {
-  currentUrl: 'https://dead-by-daylight-api-production.up.railway.app:8080/api/'
+  currentUrl: 'https://dead-by-daylight-api-production.up.railway.app/api/'
 };
